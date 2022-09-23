e33ec05f68bf133e5d9d2c21d756c1e87bd222ea
Palusijf <palusijf@yahoo.fr>
2022-09-23 15:22:37 +0200

    Second version

--- MakingCoffee.js
+++ MakingCoffee.js
@@ -1,12 +1,31 @@
-// Use "input()" to input a line from the user
-// Use "input(str)" to print some text before requesting input
-// You will need this in the following stages
 const input = require('sync-input')
+let coffeeCup = 0;
 
-console.log(`Starting to make a coffee
+function greeting() {
+    console.log(`Starting to make a coffee
 Grinding coffee beans
 Boiling water
 Mixing boiled water with crushed coffee beans
 Pouring coffee into the cup
 Pouring some milk into the cup
-Coffee is ready!`)
+Coffee is ready!`);
+}
+
+function numberOfCoffee() {
+    coffeeCup = input("Write how many cups of coffee you will need: ");
+}
+
+function ingredientNeeded(coffeeCup) {
+    let water = 200 * coffeeCup;
+    let milk = 50 * coffeeCup;
+    let coffeeBean = 15 * coffeeCup;
+
+    console.log(`For ${coffeeCup} cups of coffee you will need:
+${water} ml of water
+${milk} ml of milk
+${coffeeBean} g of coffee beans`)
+}
+
+greeting();
+numberOfCoffee();
+ingredientNeeded(coffeeCup);
