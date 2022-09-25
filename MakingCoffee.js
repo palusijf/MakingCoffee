9701d67dd8f11e871b6b44007b8cb1d1d93662a9
Palusijf <palusijf@yahoo.fr>
2022-09-25 09:42:22 +0200

    Final Step + Clean-up

--- MakingCoffee.js
+++ MakingCoffee.js
@@ -1,30 +1,11 @@
 const input = require('sync-input');
-// let coffeeCup = 0;
-// let waterNeed = 0;
-// let milkNeed = 0;
-// let coffeeBeansNeed = 0;
+
 let moneyDispo = 550;
 let waterDispo = 400;
 let milkDispo = 540;
 let cupDispo = 9;
 let coffeeBeansDispo = 120;
 let actionFlag;
-// let waterPerCup = 200;
-// let milkPerCup = 50;
-// let coffeeBeansPerCup = 15;
-// let waterCup = 0;
-// let milkCup = 0;
-// let coffeeBeansCup = 0;
-
-/*function greeting() {
-    console.log(`Starting to make a coffee
-Grinding coffee beans
-Boiling water
-Mixing boiled water with crushed coffee beans
-Pouring coffee into the cup
-Pouring some milk into the cup
-Coffee is ready!`);
-}*/
 
 function remaining() {
     console.log(`The coffee machine has:
@@ -36,10 +17,6 @@ $${moneyDispo} of money
 `);
 }
 
-/*
-function numberOfCoffee() {
-    coffeeCup = input("Write how many cups of coffee you will need: ");
-}*/
 function main() {
     do {
         action();
@@ -159,45 +136,5 @@ function takeMoney() {
     `);
 }
 
-/*function ingredientNeeded(coffeeCup) {
-    waterNeed = waterPerCup * coffeeCup;
-    milkNeed = milkPerCup * coffeeCup;
-    coffeeBeansNeed = coffeeBeansPerCup * coffeeCup;
-
-    console.log(`For ${coffeeCup} cups of coffee you will need:
-${waterNeed} ml of water
-${milkNeed} ml of milk
-${coffeeBeansNeed} g of coffee beans`);
-}*/
-
-/*function request() {
-    waterDispo = input("Write how many ml of water the coffee machine has: ");
-    milkDispo = input("Write how many ml of milk the coffee machine has: ");
-    coffeeBeansDispo = input("Write how many grams of coffee beans the coffee machine has: ");
-}*/
-
-/*function howMany(coffeeCup) {
-    waterCup = Math.floor(waterDispo / waterPerCup);
-    milkCup = Math.floor(milkDispo / milkPerCup);
-    coffeeBeansCup = Math.floor(coffeeBeansDispo / coffeeBeansPerCup);
-    let array = [waterCup, milkCup, coffeeBeansCup];
-    array.sort((a, b) => a - b);
-    let numberOfCupPossible = array[0];
-    let numberOfCupRest = numberOfCupPossible - coffeeCup;
-    if (numberOfCupRest < 0) {
-        console.log(`No, I can make only ${numberOfCupPossible} cups of coffee`);
-    } else if (numberOfCupRest === 0) {
-        console.log("Yes, I can make that amount of coffee");
-    } else {
-        console.log(`Yes, I can make that amount of coffee (and even ${numberOfCupRest} more than that)`);
-    }
-}*/
-
-//greeting();
-//request();
-
 main();
 
-//numberOfCoffee();
-//ingredientNeeded(coffeeCup);
-//howMany(coffeeCup);
